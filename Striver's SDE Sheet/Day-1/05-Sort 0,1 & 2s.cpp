3b94a81c2050b66f5e0acb73d4a7b02c159f2ea7
Manish Goyal <manishgoyalbsr7@gmail.com>
2022-06-06 12:50:07 +0530

    :)

---
+++ Striver's SDE Sheet/Day-1/05-Sort 0,1 & 2s.cpp
@@ -0,0 +1,18 @@
+void sort012(int *arr, int n)
+{
+   int lo=0, mid=0, hi=n-1;
+    while(mid<=hi){
+        if(arr[mid]==0){
+            swap(arr[lo], arr[mid]);
+            lo++; 
+            mid++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }
+       else if(arr[mid]==2){
+            swap(arr[hi], arr[mid]);
+           hi--;
+        }
+    }
+}
